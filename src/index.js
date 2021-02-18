5303c8d0f60026e51ed7765e19d53da92d0173e6
Pabizolka <1232312009qq@gmail.com>
2021-02-18 20:29:04 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arr;
+    let res = false;
+    for (let bracketsPair of bracketsConfig) {
+        arr = str.split(bracketsPair[0] + bracketsPair[1]);
+        str = arr.join("");
+    }
+    for (let bracketsPair of bracketsConfig) {
+        if (str.includes(bracketsPair[0] + bracketsPair[1])) {
+            res = check(str, bracketsConfig);
+        }
+    }
+    if (res === true) {
+        return true;
+    }
+    return str.length > 0 ? false : true;
+};
